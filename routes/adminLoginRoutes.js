b0763489f90115c8b560c700b29ca4b550762f24
Zictech Technologies <zictechng@gmail.com>
2024-02-29 17:18:44 +0000

    updated

--- routes/adminLoginRoutes.js
+++ routes/adminLoginRoutes.js
@@ -101,7 +101,7 @@ const { fetchApp } = require('../middleware/appDetails');
                 appLogo = result.app_logo
                 const logoImage = `<img src=${appLogo} width='100' height='100'/>`;
 
-                const mailBody = loginEmail(appName, 'Login Authentication', userExist.display_name, 'this is to notify you that your account has just been logged into successfully, If this is not you, contact support for immediate intervention, thank you.', logoImage)
+                const mailBody = loginEmail(appName +' Support', 'Login Authentication', userExist.display_name, 'this is to notify you that your account has just been logged into successfully, If this is not you, contact support for immediate intervention, thank you.', logoImage)
                 const TextBody = loginText(userExist.display_name,);
                 let mailOptions = {
                     from: `${appName} <noreply@ozaapp.com>`,
