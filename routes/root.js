02a108f22671b1ffe92b71e30207ba85420853ca
Zictech Technologies <zictechng@gmail.com>
2024-03-01 00:05:51 +0000

    updated

--- routes/root.js
+++ routes/root.js
@@ -6,7 +6,7 @@ const path = require('path')
 // this is for nodejs frontend view request
 router.get('^/$|/index(.html)?', (req, res) =>{
     //res.sendFile(path.join(__dirname, '..', 'views', 'index.html'))
-    res.json({success: true, message: 'Rugipo Mobile API System'})
+    res.json({success: true, message: 'Oza App API System'})
 
 })
 
