28d99314cc836744ccb1ae8a496a4abc0554dd07
Zictech <zictechng@gmail.com>
2026-09-09 12:41:10 +0100

    fix: add localhost:3002 and 192.168.1.73 to CORS allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,22 +1,36 @@
 const allowedOrigins = [
+    // Local development
     'http://localhost:2400',
     'http://localhost:3000',
     'http://localhost:3000/user',
-    'http://www.zictech-ng.com',
-    'http://localhost:4200',
-    'https://www.zictech-ng.com',
-    'http://localhost',
+    'http://localhost:3001',
+    'http://localhost:3002',  // Admin portal
+    'http://localhost:3003',
     'http://localhost:3500',
-    'https://ozawebservice.onrender.com',
+    'http://localhost:4200',
+    'http://localhost',
+
+    // Local network IPs
+    'http://192.168.1.73:3000',
+    'http://192.168.1.73:3001',
+    'http://192.168.1.73:3002',
+    'http://192.168.1.73:3003',
+    'http://192.168.1.73:3500',
     'http://192.168.1.169:3500',
     'http://192.168.1.83:3500',
+    'http://192.168.0.239:3000',
+    'http://192.168.0.239:3002',
+    'http://192.168.0.239:3500',
+
+    // Production
+    'https://ozaapp.com',
+    'https://www.ozaapp.com',
     'https://admin.ozaapp.com',
     'https://adminoffice.ozaapp.com',
     'https://ozabackendapi.ozaapp.com',
-    'https://ozaapp.com',
-    
+    'https://ozawebservice.onrender.com',
+    'http://www.zictech-ng.com',
+    'https://www.zictech-ng.com',
 ]
-
-// these are the url that we want to be able to access our api end point
 
 module.exports = allowedOrigins;
